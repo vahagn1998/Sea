f454b07b7a49b5dd73e34e30a235b641690c7e87
Vahagn Kostandyan <v.kostandyan@bk.ru>
2016-09-07 00:28:16 +0400

    MVC

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 package School.homework.SeaBattle;
 
+import School.homework.SeaBattle.Model.Game;
+
 public class Main {
     static Game game = new Game();
     public static void main(String[] args) {
